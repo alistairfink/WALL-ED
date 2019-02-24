f8adcc255fb5500f511aa0210ce5991be5802f3b
skyetheguy <mhosue@uwaterloo.ca>
2019-02-24 04:55:35 -0500

    Added initial slam implementation with hector mapping giving kind of bad occupancy grids

---
+++ src/slam/src/achilles_slam/src/test.cpp
@@ -0,0 +1,26 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "nav_msgs/OccupancyGrid.h"
+
+
+// Handle occupancy grids
+void handle_map(const nav_msgs::OccupancyGrid::ConstPtr& msg)
+{
+  ROS_INFO("Frame is [%d]", msg->header.seq);
+  ROS_INFO("Resolution is [%f]", msg->info.resolution);
+  ROS_INFO("Width is [%d]", msg->info.width);
+  ROS_INFO("Height is [%d]\n", msg->info.height);
+}
+
+int main(int argc, char **argv)
+{
+  
+  ros::init(argc, argv, "achilles_test");
+  ros::NodeHandle n;
+
+  // Subscribe to the /map for occupancy grids from Hector
+  ros::Subscriber sub = n.subscribe("map", 1000, handle_map);
+  ros::spin();
+
+  return 0;
+}

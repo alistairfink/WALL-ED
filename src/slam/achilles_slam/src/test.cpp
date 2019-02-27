692e78510ca61d6cf2785c7378e6226e40dd9243
skyetheguy <mhosue@uwaterloo.ca>
2019-02-27 00:04:00 -0500

    Wall estimation in achilles works pretty well id say

--- src/slam/achilles_slam/src/test.cpp
+++ src/slam/achilles_slam/src/test.cpp
@@ -43,6 +43,7 @@ uint32_t count_vertical_cells(const nav_msgs::OccupancyGrid::ConstPtr& msg, cons
 			count++;
 		}
 	}
+
 	return count;
 }
 
@@ -51,25 +52,37 @@ walls identify_walls(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 	walls retVal;
 
 	// Indices to search for walls
+	// Row number
 	uint32_t north_search = 0;
-	uint32_t south_search = msg->info.width;
+	uint32_t south_search = msg->info.width - 1;
+
+	// Col number
+	uint32_t east_search = msg->info.width -1;
+	uint32_t west_search = 0;
 
 	// Used to break from while loop when both walls are found
 	bool north_found = false;
 	bool south_found = false;
+	bool east_found = false;
+	bool west_found = false;
 
 	// Keeps track of count in row
 	uint32_t north_count;
 	uint32_t south_count;
+	uint32_t east_count;
+	uint32_t west_count;
 	uint32_t last_north_count = 0;
 	uint32_t last_south_count = 0;
+	uint32_t last_east_count = 0;
+	uint32_t last_west_count = 0;
 
 	// Expected wall length in cells
 	uint32_t actual_length = MAP_WIDTH / msg->info.resolution;
 
 	// Loop through rows top down and bottom up simultaneously to find N and S walls
+	// =====================================================================================
 	// Loop until both walls found or search overlaps
-	while (!north_found && !south_found && north_search < south_search)
+	while ((!north_found || !south_found) && (north_search < south_search))
 	{
 		// Search for North wall until found
 		// Will compare # occupied cells in curr row with last, if # decreases and last row had > acceptance limit THEN found
@@ -93,7 +106,7 @@ walls identify_walls(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 		// Will compare # occupied cells in curr row with last, if # decreases and last row had > acceptance limit THEN found
 		if (!south_found)
 		{
-			south_count = count_horizontal_cells(msg, north_search);
+			south_count = count_horizontal_cells(msg, south_search);
 			if (south_count > last_south_count)
 			{
 				last_south_count = south_count;
@@ -105,7 +118,73 @@ walls identify_walls(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 			
 			south_search--;
 		}
+
 	}
+	// =====================================================================================
+
+	// Loop through rows right->left and left->right simultaneously to find E and W walls
+	// =====================================================================================
+	// Loop until both walls found or search overlaps
+	while ((!east_found || !west_found) && (west_search < east_search))
+	{
+		// Search for West wall until found
+		// Will compare # occupied cells in curr col with last, if # decreases and last col had > acceptance limit THEN found
+		if (!west_found)
+		{
+			west_count = count_vertical_cells(msg, west_search);
+			if (west_count > last_west_count)
+			{
+				last_west_count = west_count;
+			}
+			else if (west_count < last_west_count && last_west_count >= actual_length / FOUND_ROW_FACTOR)
+			{
+				west_found = true;
+				retVal.west_wall = west_search - 1;
+			}
+
+			west_search++;
+		}
+
+		// Search for East wall until found
+		// Will compare # occupied cells in curr col with last, if # decreases and last col had > acceptance limit THEN found
+		if (!east_found)
+		{
+			east_count = count_vertical_cells(msg, east_search);
+			if (east_count > last_east_count)
+			{
+				last_east_count = east_count;
+			}
+			else if (east_count < last_east_count && last_east_count >= actual_length / FOUND_ROW_FACTOR)
+			{
+				east_found = true;
+				retVal.east_wall = east_search + 1;
+			}
+
+			east_search--;
+		}
+	}
+	// =====================================================================================
+
+
+	// If one wall is marked found and opposite isnt then estimate using obstacle course length
+	// ===========================================================================================
+	if (north_found && !south_found)
+	{
+		retVal.south_wall = retVal.north_wall + actual_length;
+	} else if (!north_found && south_found) 
+	{
+		retVal.north_wall = retVal.south_wall - actual_length;
+	}
+
+	if (east_found && !west_found)
+	{
+		retVal.west_wall = retVal.east_wall - actual_length;
+	} else if (!east_found && west_found) 
+	{
+		retVal.east_wall = retVal.west_wall + actual_length;
+	}
+	// ===========================================================================================
+
 
 	ROS_INFO("North wall is row [%d]", retVal.north_wall);
 	ROS_INFO("South wall is row [%d]", retVal.south_wall);
@@ -117,10 +196,10 @@ walls identify_walls(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 // Handle occupancy grids
 void handle_map(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
-	ROS_INFO("Frame is [%d]", msg->header.seq);
-	ROS_INFO("Resolution is [%f]", msg->info.resolution);
-	ROS_INFO("Width is [%d]", msg->info.width);
-	ROS_INFO("Height is [%d]\n", msg->info.height);
+	// ROS_INFO("Frame is [%d]", msg->header.seq);
+	// ROS_INFO("Resolution is [%f]", msg->info.resolution);
+	// ROS_INFO("Width is [%d]", msg->info.width);
+	// ROS_INFO("Height is [%d]\n", msg->info.height);
 
 	identify_walls(msg);
 }

2471e36b11b0228a8a21cc828724107bf8b39525
alistairfink <alistairfink@gmail.com>
2019-02-27 16:24:25 -0500

    decoding data

--- src/controls/src/motor_driver.cpp
+++ src/controls/src/motor_driver.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdint.h>
+#include <string>
+#include <sstream>
 #include "ros/ros.h"
 #include "controls/motor_control.h"
 #include "serial/serial.h"
@@ -18,25 +20,22 @@ int main(int argc, char **argv)
 		std::cout << "Connected" << std::endl;
 	}
 
-		controls::motor_control ms;
-		ms.forward = true;
-		ms.speed = 1;
-		motor_control_pub.publish(ms);
-		std::vector<unsigned char> msg_toSend (1, 0);
-		msg_toSend.insert(msg_toSend.begin() + 1, (unsigned char)(ms.speed));//(int)(ms.forward)));
-		msg_toSend.insert(msg_toSend.begin() + 1, (unsigned char)(ms.speed));
-		ser.write(msg_toSend);
-/*	for (;;)
-	{
-		std::string msg = ser.readline();
-		std::cout << msg << std::endl; 
-	}*/
-/*	for (;;)
-	{
-		controls::motor_control msg;
-		msg.forward = true;
-		msg.speed = 1;
-		motor_control_pub.publish(msg);
-	}*/
+	// Reset the arduino when it's fucking itself
+	/*while (1)
+	{*/
+	controls::motor_control mc;
+	mc.motor = 1;
+	mc.direction = 1;
+	mc.speed = 12;
+
+	std::ostringstream stm;
+	stm << mc.motor << "," << mc.direction << "," << mc.speed << "*";
+
+	std::string message = stm.str();
+	std::cout << message << " " << i << std::endl;
+
+	ser.write(message);
+	i++;
+	/*}*/
 
 }

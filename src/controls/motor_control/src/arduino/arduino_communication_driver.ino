e0c6d102001a33313b2243fb00dfeeafd4b7ff7b
alistairfink <alistairfink@gmail.com>
2019-02-27 20:34:59 -0500

    arduino motor shield lib

--- src/controls/motor_control/src/arduino/arduino_communication_driver.ino
+++ src/controls/motor_control/src/arduino/arduino_communication_driver.ino
@@ -1,4 +1,5 @@
 #include <string.h>
+#include "./DualMC33926MotorShield/DualMC33926MotorShield.h"
 
 int led = 12;
 void setup() {
@@ -29,7 +30,6 @@ void loop() {
             buff[counter] = '\0';
             motor_control[0]=atoi(strtok(buff,","));
             motor_control[1]=atoi(strtok(NULL,","));
-            motor_control[2]=atoi(strtok(NULL,","));
             dirty = true;
         }
         else
@@ -40,7 +40,7 @@ void loop() {
 
     if (dirty)
     {
-        for (int i = 0; i < motor_control[2]; i++)
+        for (int i = 0; i < motor_control[1]; i++)
         {
             digitalWrite(led, HIGH);
             delay(500);

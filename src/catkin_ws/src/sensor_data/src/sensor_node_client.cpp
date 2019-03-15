93bf91c8dade9b4514d808c44c4f105454181775
Abdulmalik Ibrahim <Malik@Abdulmaliks-MacBook-Air.local>
2019-03-15 11:41:37 -0400

    sensor_node files
    Includes all cpp and hop files for all sensors, a sensor_data package
    that includes a server and client node for ross services.

---
+++ src/catkin_ws/src/sensor_data/src/sensor_node_client.cpp
@@ -0,0 +1,46 @@
+//
+//  sensor_node_client.cpp
+//  sensor_node
+//
+//  Created by Abdulmalik Ibrahim on 2019-03-12.
+//  Copyright © 2019 Abdulmalik Ibrahim. All rights reserved.
+//
+
+#include "sensor_node_client.hpp"
+#include "ros/ros.h"
+#include "sensor_data/AddTwoInts.h"
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "sensor_node_client");
+    if (argc != 2)
+    {
+        ROS_INFO("usage: request_sensor_data");
+        return 1;
+    }
+    
+    ros::NodeHandle n;
+    ros::ServiceClient client = n.serviceClient<sensor_data::AddTwoInts>("sensor_data");
+    sensor_data::AddTwoInts srv;
+    
+    srv.request.input = atoll(argv[1]);
+    if (client.call(srv))
+    {
+		if(srv.request.input == 1)
+		{
+			ROS_INFO("response data: %ld", (long int)srv.response.hall);	
+		}
+	
+		else
+			ROS_INFO("response data: %ld", (float)srv.response.sonar);
+    }
+    
+    else
+    {
+        ROS_ERROR("Failed to call service add_two_ints");
+        return 1;
+    }
+    
+    return 0;
+}
---
+++ src/catkin_ws/src/sensor_data/src/sonar.hpp
@@ -0,0 +1,15 @@
+//
+//  sonar.hpp
+//  sensor_node
+//
+//  Created by Abdulmalik Ibrahim on 2019-03-12.
+//  Copyright © 2019 Abdulmalik Ibrahim. All rights reserved.
+//
+
+#ifndef sonar_hpp
+#define sonar_hpp
+
+#include <stdio.h>
+#include "sonar.cpp"
+float get_sonar_data();
+#endif /* sonar_hpp */

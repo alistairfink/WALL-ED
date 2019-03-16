140a9b289dcf0e9abc0636ff850c527610518485
alistairfink <alistairfink@gmail.com>
2019-03-16 16:02:35 -0400

    old tele stuff with new

--- tele_operate/src/tele_operate/src/controller.cpp
+++ tele_operate/src/tele_operate/src/controller.cpp
@@ -11,7 +11,100 @@
 #define KEYCODE_L 0x44
 #define KEYCODE_U 0x41
 #define KEYCODE_D 0x42
+#define KEYCODE_SPACE 0x20
 
+struct termios cooked, raw;
+int kfd = 0;
+
+void quit(int sig)
+{
+  (void)sig;
+  tcsetattr(kfd, TCSANOW, &cooked);
+  ros::shutdown();
+  exit(0);
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "controller");
+
+	ros::NodeHandle n;
+	ros::Publisher commands_pub = n.advertise<std_msgs::Int64>("tele_operate/command", 1);
+
+	signal(SIGINT, quit);
+
+	char c;
+	tcgetattr(kfd, &cooked);
+	memcpy(&raw, &cooked, sizeof(struct termios));
+	raw.c_lflag &=~ (ICANON | ECHO);                       
+	raw.c_cc[VEOL] = 1;
+	raw.c_cc[VEOF] = 2;
+	tcsetattr(kfd, TCSANOW, &raw);
+	puts("Reading from keyboard");
+	puts("---------------------------");
+	puts("Use arrow keys to move the WALL-ED.");
+
+	bool dirty = false;
+	while (ros::ok())
+	{  
+	    if(read(kfd, &c, 1) < 0)
+	    {
+	      perror("read():");
+	      exit(-1);
+	    }
+
+    	ROS_DEBUG("value: 0x%02X\n", c);
+
+    	std_msgs::Int64 msg;
+    	switch(c)
+    	{
+			case KEYCODE_U:
+				ROS_DEBUG("FORWARD");
+				msg.data = forward;
+				dirty = true;
+				break;
+			case KEYCODE_D:
+				ROS_DEBUG("BACK");
+				msg.data = back;
+				dirty = true;
+				break;
+			case KEYCODE_L:
+				ROS_DEBUG("LEFT");
+				msg.data = turn_left;
+				dirty = true;
+				break;
+			case KEYCODE_R:
+				ROS_DEBUG("RIGHT");
+				msg.data = turn_right;
+				dirty = true;
+				break;
+			case KEYCODE_SPACE:
+				ROS_DEBUG("SPACE");
+				msg.data = nothing;
+				dirty = true;
+				break;
+    	}
+
+
+    	if (dirty) 
+    	{
+	    	commands_pub.publish(msg);
+    		dirty = false;
+    	}
+
+		/*std_msgs::String msg;
+		std::stringstream ss;
+		ss << 1;
+		msg.data = ss.str();
+
+		ROS_INFO("%s", msg.data.c_str());
+		chatter_pub.publish(msg);
+		loop_rate.sleep()*/;
+	}
+
+	return 0;
+}
+/*
 struct termios cooked, raw;
 int kfd = 0;
 
@@ -108,3 +201,5 @@ int main(int argc, char **argv)
 
 	return 0;
 }
+
+*/

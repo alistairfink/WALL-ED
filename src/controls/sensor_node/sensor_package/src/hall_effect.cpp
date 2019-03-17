b04946f28fc94e25eb3a5ca5766da9b3e4027e09
Abdulmalik Ibrahim <Malik@Abdulmaliks-MacBook-Air.local>
2019-03-16 22:28:38 -0400

    changed includes to relative paths

--- src/controls/sensor_node/sensor_package/src/hall_effect.cpp
+++ src/controls/sensor_node/sensor_package/src/hall_effect.cpp
@@ -7,8 +7,8 @@
 //
 
 #include "hall_effect.hpp"
-#include "wiringPiI2C.h"
-#include "wiringPi.h"
+#include "./WiringPi/wiringPi/wiringPiI2C.h"
+#include "./WiringPi/wiringPi/wiringPi.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>

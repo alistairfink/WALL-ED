fd2b75460de2c7d297cf062023681e40de928e3f
Abdulmalik Ibrahim <Abdulmalikibrahim072@gmail.com>
2019-03-21 14:52:28 -0400

    Create fan_test.cpp

---
+++ src/controls/sensor_package/src/fan_test.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+  wiringPiSetup();
+  pinMode (5, OUTPUT);
+  digitalWrite(5, LOW);
+  bool a;
+
+while (1)
+{
+        cin >> a;
+        if (a)
+        {
+        cout<<"TEST HIGH"<<endl;
+        digitalWrite(5, HIGH);
+        }
+
+        if(!a)
+        {
+        cout <<"TEST LOW"<<endl;
+        digitalWrite(5, LOW);
+        }
+}  
+
+return 0;
+
+}

9c38e464b2be3d20072112446ddb3f5a8c437b52
Abdulmalik Ibrahim <Abdulmalikibrahim072@gmail.com>
2019-03-21 15:00:49 -0400

    Update hall_effect.cpp

--- src/controls/sensor_package/src/hall_effect.cpp
+++ src/controls/sensor_package/src/hall_effect.cpp
@@ -18,12 +18,16 @@ bool GPIO_setup()
     if (wiringPiSetup () == -1) //checks to make sure pi is setup correctly. 
         return 0 ;
     
-    int hallpin = 16;
-    int hallpin2 = 37;
-    int ledpin = 15;
+    int hallpin = 21;
+    int hallpin2 = 22;
+    int hallpin3 = 23;
+    int hallpin4 = 24;
+    int ledpin = 4;
     
     pinMode(hallpin, INPUT); //input is hallpin
-    pinMode(hallpin2, INPUT); 
+    pinMode(hallpin2, INPUT);
+    pinMode(hallpin3, INPUT);
+    pinMode(hallpin4, INPUT);
     pinMode(ledpin, OUTPUT); //output is LED
     digitalWrite(ledpin, 0); //turn off LED at start    
     return 1;
@@ -31,49 +35,16 @@ bool GPIO_setup()
 
 int hall_output()
 {
-    int hallpin = 16;
-    int hallpin2 = 37;
-    int ledpin = 15;
+    int hallpin = 21;
+    int hallpin2 = 22;
+    int hallpin3 = 23;
+    int hallpin4 = 24;
+    int ledpin = 4;
     
-   // if(digitalRead(hallpin) == 0) //0 input at hall_pin means magnet detected 
+   if(digitalRead(hallpin) == 0 || digitalRead(hallpin2) == 0 || digitalRead(hallpin3) == 0 || digitalRead(hallpin4) == 0 ) //0 input at hall_pin means magnet detected 
         return 1;
-  //  else
-      //  return 0;
+    else
+        return 0;
 }
-
-/******python code**********/ 
-
-/*int main (void)
- {
- //gpio.setmode(gpio.BOARD)
- //gpio.setwarnings(False)
- if (wiringPiSetup () == -1)
- return 1 ;
- 
- int hallpin = 16;
- int hallpin2 = 37;
- int ledpin = 15;
- 
- pinMode(hallpin, INPUT); //input is hallpin
- pinMode(hallpin2, INPUT);
- pinMode(ledpin, OUTPUT); //output is LED
- digitalWrite(ledpin, 0); //turn off LED at start
- 
- while(1)
- {
- if(digitalRead(hallpin) == 0)
- {
- digitalWrite(ledpin, 1);
- std::cout <<"magnet detected"<<std::endl;
- }
- 
- else
- {
- digitalWrite(ledpin, 0);
- std::cout <<"magnet not detected"<<std::endl;
- }
- }
- 
- }*/
 
 
